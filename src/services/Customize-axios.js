430dcca14c50673380b6ecb588f67c002dff400f
Võ Thành Công <76848500+V-T-Cong@users.noreply.github.com>
2024-09-17 01:47:17 +0700

    finish pagination

--- src/services/Customize-axios.js
+++ src/services/Customize-axios.js
@@ -1,4 +1,4 @@
-import axios from "./Customize-axios";
+import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://reqres.in',
@@ -8,7 +8,7 @@ const instance = axios.create({
 axios.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response.data;
+    return response;
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
